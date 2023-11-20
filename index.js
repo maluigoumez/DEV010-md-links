166df3fe64cc36e2cc7b187d6f39f99d16460475
Malui Gomez <luisagomezrodela@gmail.com>
2023-11-20 10:02:53 -0600

    extract links function working with testing names, must change after

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mdLinks = require("./lib/mdLinks.js");
 // mdLinks("./some/example.md")
 // mdLinks("./cli.js")
-mdLinks("./README.md")
+mdLinks("README.md")
   .then((links) => {
     console.log(links);
   })
